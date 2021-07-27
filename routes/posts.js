969bb093a597ff9652da7314e804afbaf2ba9b6b
Ben Chou <ben7152000@gmail.com>
2021-07-28 04:30:52 +0800

    like a post feature

--- routes/posts.js
+++ routes/posts.js
@@ -29,12 +29,12 @@ router.put('/:id', async (req, res) => {
     const post = await Post.findById(req.params.id)
     if (post.userId === req.body.userId) {
       await post.updateOne({ $set: req.body })
-      res.status(200).json('a post has been updated')
+      return res.status(200).json('a post has been updated')
     } else {
-      res.status(403).json('you can update only your post')
+      return res.status(403).json('you can update only your post')
     }
   } catch (e) {
-    res.status(500).json(e)
+    return res.status(500).json(e)
   }
 })
 
@@ -47,6 +47,22 @@ router.delete('/:id', async (req, res) => {
       return res.status(200).json('a post has been deleted')
     } else {
       return res.status(403).json('you can delete only your post')
+    }
+  } catch (e) {
+    return res.status(500).json(e)
+  }
+})
+
+// like or dislike a post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } })
+      return res.status(200).json('a post has been liked')
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } })
+      return res.status(200).json('a post has been disliked')
     }
   } catch (e) {
     return res.status(500).json(e)
