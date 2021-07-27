e50898d075881faa754290352b14db7018f583e4
Ben Chou <ben7152000@gmail.com>
2021-07-28 04:00:59 +0800

    update user

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,31 @@
 const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+// update user
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10)
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+      } catch (e) {
+        return res.status(500).json(e)
+      }
+    }
+    try {
+      await User.findOneAndUpdate(
+        req.params.id,
+        { $set: req.body }
+      )
+
+      return res.status(200).json('account hs been update')
+    } catch (e) {
+      return res.status(500).json(e)
+    }
+  } else {
+    return res.status(403).json('you can update only your account!')
+  }
+})
 
 module.exports = router
