8b88407e6ff249468eddd0c5a956f110a534a432
Ben Chou <ben7152000@gmail.com>
2021-07-28 04:19:53 +0800

    create a post feature

--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,16 @@
 const router = require('express').Router()
+const Post = require('../models/Post')
+const User = require('../models/User')
+
+// create a post
+router.post('/', async (req, res) => {
+  try {
+    const newPost = new Post(req.body)
+    const post = await newPost.save()
+    return res.status(200).json(post)
+  } catch (e) {
+    return res.status(500).json(e)
+  }
+})
 
 module.exports = router
